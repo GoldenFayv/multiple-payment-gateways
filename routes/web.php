b7baddb0dab838506ddcdc8612d57b2ea76ba7a0
GoldenFayv <favour.e2002@icloud.com>
2026-04-27 01:49:52 +0100

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/queue', function () {
+    $output = Artisan::call('queue:work', [
+        '--once' => true,
+        '--stop-when-empty' => true,
+    ]);
+
+    return response()->json([
+        'status' => 'queue processed',
+        'data' => $output
+    ]);
 });
